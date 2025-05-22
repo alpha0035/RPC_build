8c8d49ef2e4b1df60fe026c6389ce3d9ab65e4c1
Alpha <alpha07eg@gmail.com>
2025-05-22 15:43:36 +0700

    RPC ver 1.0: using tcp, ipv4, single thread

---
+++ client.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <winsock2.h>
+#include <stdlib.h>
+
+#define port 8080
+#define sever_address "127.0.0.1"
+
+struct RPCRequest {
+    int function_id;
+    int params[2];
+};
+
+struct RPCResponse {
+    int result;
+    int status_code;
+};
+
+int main(int argc, char *argv[]) {
+    WSADATA wsa;
+    SOCKET sock;
+    struct sockaddr_in server;
+    struct RPCRequest req;
+    struct RPCResponse res;
+
+    if (argc != 4) {
+        printf("Caution! Wrong input syntax! <function_id> <a> <b>\n");
+        printf("function_id: 1:add, 2:sub, 3:mul, 4:div\n");
+        return 1;
+    }
+
+    // Chuyển đổi các tham số đầu vào từ chuỗi sang số nguyên
+    req.function_id = atoi(argv[1]);
+    req.params[0] = atoi(argv[2]);
+    req.params[1] = atoi(argv[3]);
+
+    WSAStartup(MAKEWORD(2, 2), &wsa);
+
+    /*
+    Tạo socket
+    pram 1: AF_INET - giao thức IPv4
+    pram 2: SOCK_STREAM - giao thức TCP
+    pram 3: 0 - tự động chọn giao thức
+    */
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    // cấu hình địa chỉ server
+    // 127.0.0.1 là địa chỉ localhost
+    // htons() chuyển đổi số nguyên từ host byte order sang network byte order
+    server.sin_addr.s_addr = inet_addr(sever_address);
+    server.sin_family = AF_INET;
+    server.sin_port = htons(port);
+
+    // Kết nối đến server
+    connect(sock, (struct sockaddr *)&server, sizeof(server));
+
+    // Gửi yêu cầu tới server và nhận phản hồi
+    send(sock, (char *)&req, sizeof(req), 0);
+    printf("Request is sent to server: func_id=%d, a=%d, b=%d\n", req.function_id, req.params[0], req.params[1]);
+    recv(sock, (char *)&res, sizeof(res), 0);
+
+    if (res.status_code == 0) {
+        printf("Result: %d\n", res.result);
+    } else {
+        printf("Error! Bad connection.\n");
+    }
+
+    closesocket(sock);
+    WSACleanup();
+    return 0;
+}
---
+++ sever.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <winsock2.h>
+#include <conio.h>
+
+struct RPCRequest {
+    int function_id;
+    int params[2];
+};
+
+struct RPCResponse {
+    int result;
+    int status_code;
+};
+
+#define port 8080
+
+int main() {
+    WSADATA wsa;
+    SOCKET server_socket, client_socket;
+    struct sockaddr_in server, client;
+    int c, recv_size;
+    struct RPCRequest req;
+    struct RPCResponse res;
+
+    WSAStartup(MAKEWORD(2, 2), &wsa);
+
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+    // Configure server address
+    // INADDR_ANY allows the server to accept connections from any IP address
+    // htons() converts the port number from host byte order to network byte order
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(port);
+
+    // Bind the socket to the sever address and listen for incoming connections
+    bind(server_socket, (struct sockaddr *)&server, sizeof(server));
+    listen(server_socket, 3);
+    
+    // catch SIGINT signal to allow graceful shutdown
+    // signal(SIGINT, intHandler);
+    while (1) {
+        if (_kbhit()) {
+            char ch = _getch();
+            break;
+        }
+        printf("RPC is listening on port %d...\n", port);
+
+        c = sizeof(struct sockaddr_in);
+        client_socket = accept(server_socket, (struct sockaddr *)&client, &c);
+        if (client_socket == INVALID_SOCKET) {
+            printf("Refuse connection!.\n");
+            continue;
+        }
+        printf("Client connected from %s:%d\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port));
+
+        recv_size = recv(client_socket, (char *)&req, sizeof(req), 0);
+        if (recv_size == SOCKET_ERROR) {
+            printf("Receive failed!\n");
+            closesocket(client_socket);
+            continue;
+        }
+        printf("Request from client: func_id=%d, a=%d, b=%d\n", req.function_id, req.params[0], req.params[1]);
+
+        res.status_code = 0;
+        switch (req.function_id) {
+            case 1: // add
+                res.result = req.params[0] + req.params[1];
+                break;
+            case 2: // sub
+                res.result = req.params[0] - req.params[1];
+                break;
+            case 3: // mul
+                res.result = req.params[0] * req.params[1];
+                break;
+            case 4: // div
+                if (req.params[1] == 0) {
+                    res.status_code = 1;
+                    res.result = 0;
+                } else {
+                    res.result = req.params[0] / req.params[1];
+                }
+                break;
+            default:
+                res.status_code = 2;
+                res.result = 0;
+        }
+
+        send(client_socket, (char *)&res, sizeof(res), 0);
+        printf("Result is sent to client: %d (Status code: %d)\n", res.result, res.status_code);
+
+        closesocket(client_socket);
+        printf("Close connection.\n\n\n");
+    }
+
+    // Close the server socket
+    closesocket(server_socket);
+    printf("Server socket is closed\n");
+    // Cleanup Winsock
+    WSACleanup();
+    return 0;
+}
